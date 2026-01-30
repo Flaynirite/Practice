c52cb9f40de7f4f9aefecdc2f5a7c5f01290552f
Flaynirite <max.flaynifort123@gmail.com>
2026-01-30 21:27:15 +0300

    fixed app.tsx routes

--- delivery-system/src/App.tsx
+++ delivery-system/src/App.tsx
@@ -12,6 +12,11 @@ import Dashboard from "./pages/Dashboard"
 import Orders from "./pages/Orders"
 import Admin from "./pages/Admin"
 import CreateOrder from "./pages/CreateOrder"
+import Blog from "./pages/Blog"
+import FAQ from "./pages/FAQ"
+import Tracking from "./pages/Tracking"
+import Terms from "./pages/Terms"
+import Privacy from "./pages/Privacy"
 
 export default function App() {
   return (
@@ -19,8 +24,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="tracking" element={<Tracking />} />
+        <Route path="FAQ" element={<FAQ />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/guest" element={<GuestHome />} />
+        <Route path="terms" element={<Terms />} />
+        <Route path="privacy" element={<Privacy />} />
         
         {/* Захищені маршрути */}
         <Route path="/profile" element={
